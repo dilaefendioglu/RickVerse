ca67f84c924a2845acfa027370df3e9bacb53a30
Dila Efendioğlu <dilaefendioglu@gmail.com>
2026-03-17 22:58:21 +0300

    feat: implement search bar with filtering logic and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchItem, setSearchItem] = useState("");
 
   useEffect(() => {
     const loadData = async () => {
@@ -26,7 +27,16 @@ function App() {
   return (
     <div className="app-container">
       <h1 className="main-title">RickVerse</h1>
-
+      <div className="search-container">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search Caracters"
+          onChange={(e) => {
+            setSearchItem(e.target.value);
+          }}
+        />
+      </div>
       {loading ? (
         <div className="loading-container">
           <div className="loading"></div>
@@ -34,27 +44,32 @@ function App() {
         </div>
       ) : (
         <div className="card-grid">
-          {characters.map((char) => (
-            <div key={char.id} className="character-card">
-              <div className="image-wrapper">
-                <img src={char.image} alt={char.name} />
-              </div>
-              <div className="character-details">
-                <h3>{char.name}</h3>
-                <div className="status-info">
-                  {/* Dinamik class kullanımı: status alive ise yeşil, dead ise kırmızı nokta */}
-                  <span
-                    className={`status-icon ${char.status.toLowerCase()}`}
-                  ></span>
-                  <span>
-                    {char.status} - {char.species}
-                  </span>
+          {characters
+            .filter((char) =>
+              char.name.toLowerCase().includes(searchItem.toLowerCase()),
+            )
+
+            .map((char) => (
+              <div key={char.id} className="character-card">
+                <div className="image-wrapper">
+                  <img src={char.image} alt={char.name} />
                 </div>
-                <p className="location-label">Origin: {char.origin.name}</p>{" "}
-                {/*origin string değil objedir*/}{" "}
+                <div className="character-details">
+                  <h3>{char.name}</h3>
+                  <div className="status-info">
+                    {/* Dinamik class kullanımı: status alive ise yeşil, dead ise kırmızı nokta */}
+                    <span
+                      className={`status-icon ${char.status.toLowerCase()}`}
+                    ></span>
+                    <span>
+                      {char.status} - {char.species}
+                    </span>
+                  </div>
+                  <p className="location-label">Origin: {char.origin.name}</p>{" "}
+                  {/*origin string değil objedir*/}{" "}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       )}
     </div>
