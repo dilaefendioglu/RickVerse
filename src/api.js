a5816a803ecac0a300e3428d99cd22d3fa358532
Dila Efendioğlu <dilaefendioglu@gmail.com>
2026-03-20 01:19:05 +0300

    feat: add modern destructuring to api services and implement episodes support

--- src/api.js
+++ src/api.js
@@ -3,16 +3,24 @@ import axios from "axios";
 const BASE_URL = "https://rickandmortyapi.com/api";
 export const getCharacters = async (page = 1) => {
   try {
-    const response = await axios.get(`${BASE_URL}/character`, {
-      params: {
-        page,
-      },
+    const { data } = await axios.get(`${BASE_URL}/character`, {
+      params: { page },
     });
-    return response.data;
+    return data;
   } catch (error) {
     console.error("Api Hatası:", error);
     return null;
   }
 };
 
-
+export const getEpisodes = async (page = 1) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/episode`, {
+      params: { page },
+    });
+    return data;
+  } catch (error) {
+    console.error("Bölüm Yükleme Hatası:", error);
+    return null;
+  }
+};
